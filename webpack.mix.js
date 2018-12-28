e1fc336d10c50da700f219fc30e2883b82817544
Derick Vigne <derickvigne@me.com>
2018-12-27 23:49:07 -0500

    Added sass and js To Webpack Generation

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,5 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+mix.js('resources/js/clean-blog.js', 'public/js')
+   .sass('resources/sass/clean-blog.scss', 'public/css');
