4f16316114551eea1aec5fec5a4bbe91fcbe5bf1
Derick Vigne <derickvigne@me.com>
2019-01-04 03:42:51 -0500

    Modified Naming Conventions -- Added Authentication SCSS

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,6 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/clean-blog.js', 'public/js')
-   .sass('resources/sass/clean-blog.scss', 'public/css');
+mix.js('resources/js/main.js', 'public/js')
+   .sass('resources/sass/main.scss', 'public/css')
+   .sass('resources/sass/auth.scss', 'public/css');
