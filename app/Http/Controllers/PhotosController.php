28bccbfc8b59655acaf530dad6692cd17ad035d8
Derick Vigne <derickvigne@me.com>
2019-01-09 00:58:14 -0500

    Updated Pagination Chunking Limits

--- app/Http/Controllers/PhotosController.php
+++ app/Http/Controllers/PhotosController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Comments;
+use App\Photos;
 
 class PhotosController extends Controller
 {
     public function index()
     {
-        $photos = Comments::whereNotNull('photo_path')->latest()->paginate('60');
+        $photos = Photos::latest()->paginate('21');
         return view('pictures')->with('photos', $photos);
     }
 }
