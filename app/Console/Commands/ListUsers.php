9e6ad882569bca7351e1016f4df686f6a7f8ecb4
Derick Vigne <derickvigne@me.com>
2019-01-11 03:20:16 -0500

    Added List Users Command

---
+++ app/Console/Commands/ListUsers.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\User;
+
+class ListUsers extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'users:list';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Show All Active Users';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $headers = ['ID', 'First', 'Last', 'Email', 'Date Registered'];
+
+        $users = User::all(['id', 'first', 'last', 'email', 'created_at'])->toArray();
+
+        $this->table($headers, $users);
+        $this->info("Total Number of Users: " . count($users));
+    }
+}
